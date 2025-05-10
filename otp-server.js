85d4d7a8d91e6039aeaee48dce1010e5c51d1d76
Adharsh NP <gff130170@gmail.com>
2025-05-10 20:13:10 +0530

    Update CORS settings for cross-origin requests

--- otp-server.js
+++ otp-server.js
@@ -4,7 +4,11 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 let otpStore = {};
 
